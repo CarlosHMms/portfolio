1d334ea994f5a3a70522562faf3f36017c00218b
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-08-04 21:47:17 -0300

    feat: create main test class

---
+++ orcamentaJa/backend/web/src/test/java/com/orc/web/OrcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orc.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
