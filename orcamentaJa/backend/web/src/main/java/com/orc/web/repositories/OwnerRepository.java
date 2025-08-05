1e7c379625c5fd8df593614ad216576fe6cbed56
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-08-04 21:41:45 -0300

    feat: add Owner repo

---
+++ orcamentaJa/backend/web/src/main/java/com/orc/web/repositories/OwnerRepository.java
@@ -0,0 +1,10 @@
+package com.orc.web.repositories;
+
+import com.orc.web.entities.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+
+}
