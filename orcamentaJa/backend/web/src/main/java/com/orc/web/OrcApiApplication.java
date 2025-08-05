0032d7b51af4acd5fabd44d16371714400f252c7
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-08-04 21:45:23 -0300

    feat: add spring application main class

---
+++ orcamentaJa/backend/web/src/main/java/com/orc/web/OrcApiApplication.java
@@ -0,0 +1,13 @@
+package com.orc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcApiApplication.class, args);
+	}
+
+}
