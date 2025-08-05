78601ab2db135cb9ae018ba17403b79f5f28a4f8
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-08-04 21:13:47 -0300

    feat: add app component

---
+++ orcamentaJa/frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Spreadsheet } from './pages/spreadsheet/spreadsheet';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Spreadsheet,
+  },
+];
